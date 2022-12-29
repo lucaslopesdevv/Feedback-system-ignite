45dbc0d617b6ac81fc596fb0ff547ecb7dd9e4ac
Lucas Fabricio Lopes <94392411+lucaslopesdevv@users.noreply.github.com>
2022-12-28 22:25:38 -0300

    New Commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css";
 
@@ -10,12 +11,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1536104968055-4d61aa56f46a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=50"
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/lucaslopesdevv.png"
-          alt=""
-        />
-
+        <Avatar src="https://github.com/lucaslopesdevv.png" />
         <strong>Lucas Fabricio Lopes</strong>
         <span>Web Developer</span>
       </div>
